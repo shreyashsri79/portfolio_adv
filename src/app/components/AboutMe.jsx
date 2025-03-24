ea7f9b94713c8c8d60c8c5e35271ec6b8b4a4a7a
Shreyash Neeraj <shreyashsri79@gmail.com>
2025-03-24 10:45:04 +0530

    day1

---
+++ src/app/components/AboutMe.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Happy_Monkey, Orbit} from "next/font/google";
+import { Orbitron } from "next/font/google";
+
+const fontTitle = Orbitron({ subsets: ["latin"], weight: ["400", "400"] })
+const fontDesc = Happy_Monkey({ subsets: ["latin"], weight: ["400", "400"] })
+
+const AboutMe = () => {
+  const [flipped, setFlipped] = useState(false);
+
+  return (
+    <div
+      className="pointer-events-auto relative w-[400px] h-[150px] md:w-[300px] md:h-[250px] perspective-1000"
+      onMouseEnter={() => setFlipped(true)}
+      onMouseLeave={() => setFlipped(false)}
+    >
+      <motion.div
+        className="absolute w-full h-full cursor-pointer"
+        animate={{ rotateY: flipped ? 180 : 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        style={{ transformStyle: "preserve-3d" }}
+      >
+        {/* Front Side */}
+        <div className={` ${fontTitle.className} absolute w-full h-full bg-white/10 backdrop-blur-xs rounded-lg shadow-lg flex justify-center items-center text-white text-2xl font-bold backface-hidden -tracking-tighter`}>
+          About Me
+        </div>
+
+        {/* Back Side */}
+        <div className="absolute w-full h-full bg-white/10 backdrop-blur-xs rounded-lg shadow-lg flex justify-center items-center text-black text-sm p-4 rotate-y-180 backface-hidden">
+          <p className={`${fontDesc.className} text-lg font-bold text-amber-50`}>
+            Oh, me ??💀 <br /> <b>BOLD choice</b> <br /><b>BAD choice</b> 
+          </p>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default AboutMe;
--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,45 @@
-import Image from "next/image";
 
+import Spline from "@splinetool/react-spline/next";
+import { Major_Mono_Display } from "next/font/google";
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import { Tiny5 } from "next/font/google";
+
+const font = Major_Mono_Display({ subsets: ["latin"], weight: ["400", "400"] })
+const tiny5 = Tiny5({ subsets: ["latin"], weight: ["400", "400"] })
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <>
+      <div className="relative h-screen w-screen inset-0">
+        <div className="absolute h-full w-full pointer-events-auto">
+          <main className="h-full w-full">
+            <Spline
+              scene="https://prod.spline.design/Y3dADZBW0b0kwLub/scene.splinecode" 
+              className="no-highlight h-full w-full"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </main>
+        </div>    
+        <div className={`${font.className} py-15 sm:py-30 absolute inset-0 -z-10 flex justify-center sm:items-start text-center font-extrabold -tracking-tighter text-7xl sm:text-8xl md:text-9xl pointer-events-none`}>
+          SHReYAsH NeeRAJ
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+
+      {/* Hides the "Made with Spline" logo (DO NOT Question my morals ) */}
+      <div className="absolute bottom-0 right-0 bg-[#0a0a0a] h-20 w-50 flex justify-center items-center " />
+
+
+
+      <div className=" z-10 pointer-events-none m-5 absolute inset-0 flex flex-wrap md:flex-wrap-reverse  justify-center items-start gap-x-20 ">
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
+
+      <div className={` ${tiny5.className} p-20 flex justify-center w-screen text-3xl`}>Use a mouse plizzz 👉👈</div>
+      
+    </>
   );
 }
